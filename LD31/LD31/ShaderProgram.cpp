eb31f4144b87fde9b93db177de86dfe01925d65a
Mramaizng <beersbr@gmail.com>
2014-12-05 20:17:43 -0800

    updated compile shader

--- LD31/LD31/ShaderProgram.cpp
+++ LD31/LD31/ShaderProgram.cpp
@@ -5,11 +5,90 @@ std::map<std::string, ShaderProgram> ShaderProgram::shaders;
 
 ShaderProgram::ShaderProgram(void)
 {
-
+	shaderID = -1;
 }
 
 
 ShaderProgram::~ShaderProgram(void)
 {
 
+}
+
+bool ShaderProgram::CompileShader(ShaderProgram shader, const bool debug)
+{
+	shader.vertexShaderCode = ShaderProgram::ReadFile(shader.vertexShaderPath);
+	shader.fragmentShaderCode = ShaderProgram::ReadFile(shader.fragmentShaderPath);
+
+	GLuint vertexProgramID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragmentProgramID = glCreateShader(GL_FRAGMENT_SHADER);;
+
+	int compileVertexLogLength = 0;
+	int compileFragmentLogLength = 0;
+
+	std::vector<char> vectorCompileLogMessage = std::vector<char>(0);
+	std::vector<char> fragmentCompileLogMessage = std::vector<char>(0);
+
+	GLint vertexCompileResult = GL_FALSE;
+	GLint fragmentCompileResult = GL_FALSE;
+
+	const char* vertexSource = shader.vertexShaderCode.c_str();
+	const char* fragmentSource = shader.fragmentShaderCode.c_str();
+
+
+	// check if the vertex shader compile ok
+	glShaderSource(vertexProgramID, 1, &vertexSource, NULL);
+	glCompileShader(vertexProgramID);
+
+	glGetShaderiv(vertexProgramID, GL_COMPILE_STATUS, &vertexCompileResult);
+	glGetShaderiv(vertexProgramID, GL_INFO_LOG_LENGTH, &compileVertexLogLength);
+	vectorCompileLogMessage.resize(compileVertexLogLength);
+	glGetShaderInfoLog(vertexProgramID, compileVertexLogLength, NULL, &vectorCompileLogMessage[0]);
+
+	fprintf(stdout, " -- VERTEX SHADER COMPILED (%d)\n%s\n", vertexCompileResult, &vectorCompileLogMessage[0]);
+
+	if(vertexCompileResult == GL_FALSE)
+		return false;
+
+	
+
+	// check if the fragment shader compile ok
+	glShaderSource(fragmentProgramID, 1, &vertexSource, NULL);
+	glCompileShader(fragmentProgramID);
+
+	
+	glGetShaderiv(fragmentProgramID, GL_COMPILE_STATUS, &vertexCompileResult);
+	glGetShaderiv(fragmentProgramID, GL_INFO_LOG_LENGTH, &compileVertexLogLength);
+	vectorCompileLogMessage.resize(compileVertexLogLength);
+	glGetShaderInfoLog(fragmentProgramID, compileVertexLogLength, NULL, &vectorCompileLogMessage[0]);
+
+	fprintf(stdout, " -- VERTEX SHADER COMPILED (%d)\n%s\n", vertexCompileResult, &vectorCompileLogMessage[0]);
+
+	if(vertexCompileResult == GL_FALSE)
+		return false;
+
+
+	return true;
+}
+
+std::string ShaderProgram::ReadFile(std::string const filePath)
+{
+	std::fstream fileStream(filePath.c_str(), std::ios::in);
+
+	if(!fileStream.is_open())
+	{
+		fprintf(stdout, "Could not open file at path %s\n", filePath);
+		return std::string("ERROR");
+	}
+
+	std::string data = "";
+	std::string line = "";
+	while(std::getline(fileStream, line))
+	{
+		data += (line + "\n");
+	}
+
+	fileStream.close();
+
+	return data;
+	
 }
--- LD31/LD31/ShaderProgram.h
+++ LD31/LD31/ShaderProgram.h
@@ -4,7 +4,10 @@
 #include <string>
 #include <map>
 #include <algorithm>
+#include <fstream>
+#include <vector>
 
+#include <GL/glew.h>
 #include <SDL_opengl.h>
 
 class ShaderProgram
@@ -13,8 +16,17 @@ public:
 	ShaderProgram(void);
 	~ShaderProgram(void);
 
+	static bool CompileShader(ShaderProgram shader, const bool debug = false);
+	static std::string ReadFile(std::string const filePath);
+
 public:
 	GLuint shaderID;
+
+	std::string vertexShaderCode;
+	std::string fragmentShaderCode;
+
+	std::string vertexShaderPath;
+	std::string fragmentShaderPath;
 
 	static std::map<std::string, ShaderProgram> shaders;
 };
