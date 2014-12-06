c56bc590a086f19f2e76f72d773a9c527eb0a0cb
Mramaizng <beersbr@gmail.com>
2014-12-05 18:43:30 -0800

    simple window

--- LD31/LD31/main.cpp
+++ LD31/LD31/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <SDL.h>
 
 int main(int argc, char* argv[])
 {
+	
+	if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		return 0;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("TITLE", 0, 0, 100, 100, 0);
+
 	return 0;
 }
