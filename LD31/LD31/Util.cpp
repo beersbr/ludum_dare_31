348e9ee8b1ed14278cdafdb4d384869a647d957b
Mramaizng <beersbr@gmail.com>
2014-12-06 19:19:26 -0800

    updated util
    updated util

--- LD31/LD31/Util.cpp
+++ LD31/LD31/Util.cpp
@@ -37,8 +37,6 @@ GLuint SDL_SurfaceToTexture(SDL_Surface *surface) {
     //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); 
     //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-	
-
     glTexImage2D(GL_TEXTURE_2D, 0, ncolors, s->w, s->h, 0,
             GL_RGBA8, GL_UNSIGNED_BYTE, s->pixels);
  
--- LD31/LD31/main_vertex.glsl
+++ LD31/LD31/main_vertex.glsl
@@ -2,17 +2,19 @@
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 color;
 layout(location = 2) in vec3 normal;
+layout(location = 3) in vec2 uv;
 
 uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 model;
 
+
 // uniform vec3 dirLight0;
 
-out vec3 fragColor;
+out vec2 UVc;
 
 void main()
 {
-	fragColor = color;
+	UVc = uv;
 	gl_Position = projection * view * model * vec4(position, 1.0);
 }
