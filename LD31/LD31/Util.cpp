9b0aff29350a5a73b8cd90b0ec74539ee8dea945
Mramaizng <beersbr@gmail.com>
2014-12-06 19:18:57 -0800

    testing util
    updaed code when debugging

--- LD31/LD31/Util.cpp
+++ LD31/LD31/Util.cpp
@@ -30,8 +30,8 @@ GLuint SDL_SurfaceToTexture(SDL_Surface *surface) {
     glGenTextures(1, &tid);
     glBindTexture(GL_TEXTURE_2D, tid);
 	int err2 = glGetError();
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); 
@@ -40,7 +40,7 @@ GLuint SDL_SurfaceToTexture(SDL_Surface *surface) {
 	
 
     glTexImage2D(GL_TEXTURE_2D, 0, ncolors, s->w, s->h, 0,
-            texture_format, GL_UNSIGNED_BYTE, s->pixels);
+            GL_RGBA8, GL_UNSIGNED_BYTE, s->pixels);
  
     return tid;
 }
